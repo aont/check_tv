5cb163945cb612c37090eab61cb3e1f97f5e7ec9
aont <>
2019-10-02 12:31:00 +0900

    filtering

--- sandbox.py
+++ sandbox.py
@@ -9,33 +9,6 @@ import os
 import requests
 import urllib.parse
 
-class LINE(object):
-    def __init__(self, sess, line_notify_token):
-        self.sess = sess
-        self.line_notify_token = line_notify_token
-        self.line_notify_api = u'https://notify-api.line.me/api/notify'
-        self.headers = {u'Authorization': u'Bearer ' + line_notify_token}
-
-    def notify(self, message):
-        # print message
-        # return
-        sys.stderr.write(u'[info] line notify: %s\n' % message)
-        for t in range(5):
-            try:
-                line_notify = self.sess.post(self.line_notify_api, data = {u'message': message}, headers = self.headers)
-                if requests.codes.ok != line_notify.status_code:
-                    sys.stderr.write(u"[info] line status_code = %s\n" % line_notify.status_code)
-                    sys.stderr.write(u"[info] wait for 5s and retry\n")
-                    # sys.stderr.flush()
-                    time.sleep(5)
-                    continue
-
-                break
-            except requests.exceptions.ConnectionError as e:
-                sys.stderr.write(u"[warn] LINE ConnectionError occured. retrying...\n")
-                sys.stderr.write(traceback.format_exc())
-                # sys.stderr.flush()
-                continue
 
 def str_abbreviate(str_in):
     len_str_in = len(str_in)
@@ -81,7 +54,7 @@ def keyword2rss(keyword_list):
         # rss_list.append(base_url + "&condition.keyword=%s&stationPlatformId=%s" % (urllib.parse.quote(keyword, safe=''), 2))
     return rss_list
 
-def filter_channel(summary):
+def check_channel(summary):
     filter_channels = ["ＮＨＫ総合１・東京(Ch.1)","ＮＨＫＥテレ１・東京(Ch.2)","日テレ(Ch.4)","テレビ朝日(Ch.5)","ＴＢＳ(Ch.6)","テレビ東京(Ch.7)","フジテレビ(Ch.8)","ＴＯＫＹＯ　ＭＸ１(Ch.9)",\
         "ＮＨＫ ＢＳ１(Ch.1)","ＮＨＫ ＢＳプレミアム(Ch.3)","ＢＳ日テレ(Ch.4)","ＢＳ朝日(Ch.5)","ＢＳ-ＴＢＳ(Ch.6)","ＢＳテレ東(Ch.7)","ＢＳフジ(Ch.8)","BS11イレブン(Ch.11)","BS12 トゥエルビ(Ch.12)","BSキャンパスex"]
     for channel in filter_channels:
@@ -90,57 +63,19 @@ def filter_channel(summary):
             return True
     return False
 
-def filter_program(entry):
-    if entry.title == u"NHKニュース　おはよう日本":
-        return False
-    return True
-
 if __name__ == u'__main__':
 
-    line_sess = requests.session()
-    line_notify_token = os.environ[u'LINE_TOKEN']
-    line = LINE(line_sess, line_notify_token)
-
-    tv_sonet_base = u"https://tv.so-net.ne.jp/rss/schedulesBySearch.action?condition.genres%5B0%5D.parentId=-1&condition.genres%5B0%5D.childId=-1&submit=%E6%A4%9C%E7%B4%A2&stationAreaId=23&submit.x=&submit.y="
-
-
-    pg_url = os.environ[u'DATABASE_URL']
-    table_name = u'generic_text_data'
-    key_name = u'check_tv'
-    pg_conn = psycopg2.connect(pg_url)
-    pg_cur = pg_conn.cursor()
-
-    check_tv_data = pg_init_json(pg_cur, table_name, key_name)
-    checked_previously = check_tv_data.get(u"checked_previously")
-    if checked_previously is None:
-        checked_previously = []
-    checked_thistime = []
-
-    keyword_list = check_tv_data.get(u"keyword_list")
-    if keyword_list is None:
-        keyword_list = []
-
-    for rssurl in keyword2rss(keyword_list):
+    for rssurl in ['https://tv.so-net.ne.jp/rss/schedulesBySearch.action?stationPlatformId=0&condition.keyword=%E3%83%A9%E3%83%B4%E3%82%A7%E3%83%AB&submit=%E6%A4%9C%E7%B4%A2&stationAreaId=23&submit.x=&submit.y=']:
         sys.stderr.write(u'[info] rss=%s\n' % rssurl) 
         d = feedparser.parse(rssurl)
 
+        # sys.stderr.write("%s\n" % d)
         for entry in d['entries']:
-            checked_thistime.append(entry.link)
-            if entry.link in checked_previously:
-                sys.stderr.write("[info] skipping %s (checked previously)\n" % (entry.title))
+            if not check_channel(entry.summary):
+                sys.stderr.write("[warn] skipping %s (%s)\n" % (entry.title, entry.summary))
                 continue
-            elif not filter_channel(entry.summary):
-                sys.stderr.write("[info] skipping  %s (%s) (channnel is filtered)\n" % (entry.title, entry.summary))
-                continue
-            elif not filter_program(entry):
-                sys.stderr.write("[info] skipping  %s (program is filtered)\n" % (entry.title))
-                continue
-            else:
-                line.notify(u"%s\n%s" % (entry.title, entry.link))
+            for item in entry.items():
+                sys.stderr.write(u'[info] %s=%s\n' % (item[0], item[1]))
+            # if entry.link not in checked_previously:
+            #     sys.stderr.write(u"%s\n" % (entry.title, entry.link))
         
-    check_tv_data[u'checked_previously'] = checked_thistime
-    pg_update_json(pg_cur, table_name, key_name, check_tv_data)
-    
-    pg_cur.close()
-    pg_conn.commit()
-    pg_conn.close()
